00f2ee421b8b266816f4673052e451bc09a73438
P1CACHU <p1cachu@yandex.ru>
2017-11-24 19:55:44 +0300

    первая сцена и класс

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+	[SerializeField] private Slider _health;
+
+	public void OnRockClicked()
+	{
+	}
+
+	public void OnScissorClicked()
+	{
+	}
+
+	public void OnPaperClicked()
+	{
+	}
+}
