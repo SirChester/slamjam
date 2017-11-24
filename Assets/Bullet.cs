26de1f45def1537a05ffb52f3f18b522858c6c28
P1CACHU <p1cachu@yandex.ru>
2017-11-24 21:00:25 +0300

    левый стреляет в правого красным квадратом

---
+++ Assets/Bullet.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+	private void Awake()
+	{
+		GetComponent<Rigidbody2D>().AddForce(Vector2.right * 10000.0f);
+	}
+
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		Destroy(gameObject);
+//		if (coll.gameObject.tag == "Enemy")
+//			coll.gameObject.SendMessage("ApplyDamage", 10);
+	}
+}
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,9 +4,11 @@ using UnityEngine.UI;
 public class GameController : MonoBehaviour
 {
 	[SerializeField] private Slider _health;
-
+	[SerializeField] private RectTransform _room;
+	
 	public void OnRockClicked()
 	{
+		Instantiate(Resources.Load("Prefabs/Rock"), _room);
 	}
 
 	public void OnScissorClicked()
