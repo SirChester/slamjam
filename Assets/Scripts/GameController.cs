bbb2d0bc1992408c73b5ea9796fdf5e64bccb8f2
P1CACHU <p1cachu@yandex.ru>
2017-11-25 13:02:11 +0300

    лаг на ходьбу

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,16 +1,18 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
 	[SerializeField] private Slider _playerHealth;
 	[SerializeField] private Slider _enemyHealth;
-	[SerializeField] private RectTransform _myPlace;
-	[SerializeField] private RectTransform _enemyPlace;
 	[SerializeField] private Player _player;
 	[SerializeField] private Enemy _enemy;
 	[SerializeField] private Floor _playerFloor;
 	[SerializeField] private Floor _enemyFloor;
+
+	private bool _playerMovementLocked;
+	private bool _enemyMovementLocked;
 
 	private void Awake()
 	{
@@ -49,14 +51,27 @@ public class GameController : MonoBehaviour
 
 	public void OnScissorClicked(bool enemy)
 	{
-		var obj = Instantiate(Resources.Load("Prefabs/Scissor"), enemy ? _enemyPlace : _myPlace) as GameObject;
-		obj.GetComponent<Bullet>().PushTo(!enemy);
+		if (enemy)
+		{
+			_enemy.ShootScissor();
+		}
+
+		else
+		{
+			_player.ShootScissor();
+		}
 	}
 
 	public void OnPaperClicked(bool enemy)
 	{
-		var obj = Instantiate(Resources.Load("Prefabs/Paper"), enemy ? _enemyPlace : _myPlace) as GameObject;
-		obj.GetComponent<Bullet>().PushTo(!enemy);
+		if (enemy)
+		{
+			_enemy.ShootPaper();
+		}
+		else
+		{
+			_player.ShootPaper();
+		}
 	}
 
 	private void Update()
@@ -69,41 +84,59 @@ public class GameController : MonoBehaviour
 
 	private void UpdatePlayerMovement()
 	{
+		if (_playerMovementLocked)
+		{
+			return;
+		}
+		
 		if (Input.GetKeyDown(KeyCode.W))
 		{
 			_player.Up();
+			StartCoroutine(LockPlayerMovement());
 		}
 		if (Input.GetKeyDown(KeyCode.A))
 		{
 			_player.Left();
+			StartCoroutine(LockPlayerMovement());
 		}
 		if (Input.GetKeyDown(KeyCode.S))
 		{
 			_player.Down();
+			StartCoroutine(LockPlayerMovement());
 		}
 		if (Input.GetKeyDown(KeyCode.D))
 		{
 			_player.Right();
+			StartCoroutine(LockPlayerMovement());
 		}
 	}
 	
 	private void UpdateEnemyMovement()
 	{
+		if (_enemyMovementLocked)
+		{
+			return;
+		}
+		
 		if (Input.GetKeyDown(KeyCode.UpArrow))
 		{
 			_enemy.Up();
+			StartCoroutine(LockEnemyMovement());
 		}
 		if (Input.GetKeyDown(KeyCode.LeftArrow))
 		{
 			_enemy.Left();
+			StartCoroutine(LockEnemyMovement());
 		}
 		if (Input.GetKeyDown(KeyCode.DownArrow))
 		{
 			_enemy.Down();
+			StartCoroutine(LockEnemyMovement());
 		}
 		if (Input.GetKeyDown(KeyCode.RightArrow))
 		{
 			_enemy.Right();
+			StartCoroutine(LockEnemyMovement());
 		}
 	}
 	
@@ -137,5 +170,19 @@ public class GameController : MonoBehaviour
 		{
 			OnPaperClicked(true);
 		}
+	}
+
+	private IEnumerator LockPlayerMovement()
+	{
+		_playerMovementLocked = true;
+		yield return new WaitForSeconds(1.0f);
+		_playerMovementLocked = false;
+	}
+	
+	private IEnumerator LockEnemyMovement()
+	{
+		_enemyMovementLocked = true;
+		yield return new WaitForSeconds(1.0f);
+		_enemyMovementLocked = false;
 	}
 }
