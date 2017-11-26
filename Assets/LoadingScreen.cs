300c75aacaba8cae651766020ce6244d1d83ea5d
P1CACHU <p1cachu@yandex.ru>
2017-11-26 12:58:34 +0300

    логотип

---
+++ Assets/LoadingScreen.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class LoadingScreen : MonoBehaviour
+{
+	[SerializeField] private GameObject _logo;
+	[SerializeField] private GameObject _start;
+	[SerializeField] private GameObject _main;
+
+	private void Awake()
+	{
+		StartCoroutine(WaitForLogoEnd());
+	}
+
+	private IEnumerator WaitForLogoEnd()
+	{
+		yield return new WaitForSeconds(1.2f);
+		Destroy(_logo);
+		_start.SetActive(true);
+		_main.SetActive(true);
+	}
+}
